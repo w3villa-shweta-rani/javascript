cbec1e75c4fbbb721f5934dc4c369882e8796303
w3villa-shweta-rani <shweta.rani@w3villa.com>
2025-10-21 18:40:11 +0530

    initial commit

---
+++ day01.js
@@ -0,0 +1,41 @@
+console.log("javascript loaded");
+// declearation 
+console.log(z);
+let m = 10
+// alert("welcome to javascript");
+
+let x = 5;
+a();
+b();
+
+console.log(x);
+function a (){
+    let x=10;
+    console.log(x)
+}
+
+function b (){
+    let x=100;
+    console.log(x)
+}
+
+function c (){
+    const x = 76;
+    console.log(x)
+}
+c()
+
+// the scope chain 
+
+function a() {
+    const b=10;
+    c();
+    function c(){
+        console.log(b)
+
+    }
+}
+a();
+// console.log(b);
+
+
