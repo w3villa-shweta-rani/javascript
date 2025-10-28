bb669dc98eebfffee3bac0331eb9f134223aa407
w3villa-shweta-rani <shweta.rani@w3villa.com>
2025-10-28 12:38:53 +0530

    day 6 work

---
+++ day06.js
@@ -0,0 +1,80 @@
+// control structure is used to control the flow of the code
+
+// if code is true than code will run only
+let num = 20;
+if (num > 18 ){
+    console.log("you are eligible for voting")
+}
+
+//if else (if condition is not satisfied than will run)
+let age = 22;
+if ( age > 18){
+    console.log("congrats, you are eligible for voting")
+}
+else {
+    console.log("you are not eligible for voting")
+}
+
+//if....elseif....else
+const number = 0;
+if (number > 0 ){
+    console.log("number is positive");
+    
+}
+else if(number == 0){
+    console.log("number is equal")
+}
+else {
+    console.log("number is negative")
+}
+
+//switch when a variable has multiple values 
+
+const day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday")
+        break;
+    case 2:
+        console.log("tuesday") 
+        break; 
+    case 3:
+        console.log("wednesday")
+        break;
+    case 4:
+        console.log("thursday")  
+        break; 
+    case 5:
+        console.log("Friday") 
+        break;   
+    case 6:
+        console.log("saturday")   
+        break;
+    case 7:
+        console.log("sunday") 
+        break;
+    default:
+        console.log("Invalid day");        
+}
+//Looping
+
+//for loop
+for(let i=1; i<=4; i++){
+    console.log(i);
+}
+
+//while loop
+let i = 2;
+while(i <= 6){
+    console.log(i);
+    i++;
+}
+
+//for loop
+for(let i=1; i<=7; i++){
+    co
+}
+
+
+
