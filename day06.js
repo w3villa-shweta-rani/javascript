e4160ead732879701265c211c5d05848a7f65b8d
w3villa-shweta-rani <shweta.rani@w3villa.com>
2025-10-28 18:22:21 +0530

    updated javascript events example

--- day06.js
+++ day06.js
@@ -70,11 +70,86 @@ while(i <= 6){
     console.log(i);
     i++;
 }
+// do while loop
+let j =1;
+do{
+    console.log("hello")
+    i++;
+}while (i <= 3);
 
-//for loop
-for(let i=1; i<=7; i++){
-    co
+// for of loop (Array yaa string py iterate krny k liye)
+let fruits = ["apple","bnana","mango"];
+for(let fruit of fruits){
+    console.log(fruit);
 }
 
+//file the n fruit is present or not in fruits array
+for(let fruit of fruits){
+    if(fruit == "apple"){
+        console.log(`${fruit} is available in our cart`)
+    }
+}
 
+// for each loop(sirf array py use hota hai)
+let nums = [1,2,3,4,5,6,7,8,9];
+nums.forEach(function(n){
+    console.log(n);
+});
 
+//Functions in javascript
+function add (a,b){
+    return a + b;
+}
+console.log(add(5,3));
+
+const addition = (a,b) => {
+
+}
+
+// Function Expression in javascript
+const multiply = function(x, y){
+    return x * y;
+    
+}
+console.log(multiply(3, 4));
+
+//Arrow FunctionFunction
+const add1 = (a, b) =>  a + b;
+console.log(add1(3, 3));
+
+//Parameters and return
+function greet(name) {
+    return "Hello, "+ name;
+}
+console.log(greet("shweta"));
+
+//scope (global scope and local scope)
+//global scope: functionk bahar declared variable
+//Local variable: function k andar declared variable
+
+let x = 10;
+function test() {
+    let y = 5;
+    console.log(x + y);
+    
+}
+test();
+
+// closure
+function outer() {
+    let count = 0;
+    function inner() {
+        count++;
+        console.log(count);
+    }
+    return inner;
+
+}
+const counter = outer();
+counter();
+counter();
+
+//event object
+document.addEventListener("click",function(event){
+    
+})
